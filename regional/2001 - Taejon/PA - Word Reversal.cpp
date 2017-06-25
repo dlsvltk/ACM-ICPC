abf50b5c16134a7c088abc34cf6198a545491ef6
dlsvltk <ik_me@naver.com>
2017-06-25 18:43:04 +0900

    Update and rename PA - Word Reversal to PA - Word Reversal.cpp

--- regional/2001 - Taejon/PA - Word Reversal.cpp
+++ regional/2001 - Taejon/PA - Word Reversal.cpp
@@ -1,17 +1,16 @@
 #include<stdio.h>
 int main(){
     int t;
-    for(scanf("%d", &t);t--;){
+    for(scanf("%d ", &t);t--;){
         char a[1001];
         gets(a);
         int i,j;
         for(j=i=0;a[i];i++){
             if(a[i] == ' '){
                 int k;
-                for(k=i-1;k> j;k--)putchar(a[k]);
+                for(k=i-1;k>=j;k--)putchar(a[k]);
                 putchar(' ');
                 j = i+1;
-                
             }
         }
         int k;
