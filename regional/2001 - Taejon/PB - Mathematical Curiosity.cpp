d29e1173caa8e2e624c64607819555d01ade5e09
dlsvltk <ik_me@naver.com>
2017-06-25 18:54:48 +0900

    Update PB - Mathematical Curiosity.cpp

--- regional/2001 - Taejon/PB - Mathematical Curiosity.cpp
+++ regional/2001 - Taejon/PB - Mathematical Curiosity.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main(){   int n;   int a, b;   int cnt = 0;
     cin >> n;
     for (int i = 0; i < n; i++)   {
-		cin >> a >> b;
+        cin >> a >> b;
         for (float x = 1; x < a; x++)    for (float y = x + 1; y < a; y++)    (float)((x*x + y*y + b) / (x*y)) == (int)((x*x + y*y + b) / (x*y)) ? cnt++ : 0;
         cout << cnt << endl;      cnt = 0;
     }
